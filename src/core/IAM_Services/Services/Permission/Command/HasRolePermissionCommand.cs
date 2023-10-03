902c6f861f4c5ef28e60636213260b6d2e95aec5
Amir <96792239+AmBplus@users.noreply.github.com>
2023-10-03 12:42:20 +0330

    Fix Errors Compile Project

--- src/core/IAM_Services/Services/Permission/Command/HasRolePermissionCommand.cs
+++ src/core/IAM_Services/Services/Permission/Command/HasRolePermissionCommand.cs
@@ -8,41 +8,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AccessManagement.Services.Permission.Command
+namespace AccessManagement.Services.Permission.Command;
+
+public class HasRolePermissionCommandRequest : IRequest<ResultOperation>
 {
-    public class HasRolePermissionCommandRequest : IRequest<ResultOperation>
-    {
-        public string PermissionName { get; set; }
-        public string RoleName { get; set; }    
+    public string PermissionName { get; set; }
+    public string RoleName { get; set; }    
 
-    }
-    public class HasRolePermissionCommandHandler : IRequestHandler<HasRolePermissionCommandRequest, ResultOperation>
-    {
-        IAccessManagementDbContext Context;
+}
+public class HasRolePermissionCommandHandler : IRequestHandler<HasRolePermissionCommandRequest, ResultOperation>
+{
+    IAccessManagementDbContext Context;
 public HasRolePermissionCommandHandler(IAccessManagementDbContext context)
-        {
-            Context = context;
-        }
-
-        public async Task<ResultOperation> Handle(HasRolePermissionCommandRequest request, CancellationToken cancellationToken)
-        {
-
-            var role = await Context.Roles
-                .Where(x => x.Name == request.RoleName).Include(x => x.Permissions).SingleOrDefaultAsync();
-            if(role == null)
-            {
-                throw new Exception("UnValid Request");
-            }
-            var flag = role.Permissions.Any(x=>x.Name == request.PermissionName);
-            if (flag)
-            {
-                return ResultOperation.ToFailedResult("نقش به این رول دسترسی ندارد");
-            }
-
-            return ResultOperation.ToSuccessResult();
-            
-        }
-    }
     {
+        Context = context;
+    }
+
+    public async Task<ResultOperation> Handle(HasRolePermissionCommandRequest request, CancellationToken cancellationToken)
+    {
+
+        var role = await Context.Roles
+            .Where(x => x.Name == request.RoleName).Include(x => x.Permissions).SingleOrDefaultAsync();
+        if(role == null)
+        {
+            throw new Exception("UnValid Request");
+        }
+        var flag = role.Permissions.Any(x=>x.Name == request.PermissionName);
+        if (flag)
+        {
+            return ResultOperation.ToFailedResult("نقش به این رول دسترسی ندارد");
+        }
+
+        return ResultOperation.ToSuccessResult();
+        
     }
 }
+
+
+
